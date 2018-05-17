75f802b41e2224d5198d5f1fb6f4854332431b20
darwinfv <dvaz@purdue.edu>
2018-05-16 21:10:05 -0400

    Improved scraper

--- scraper.js
+++ scraper.js
@@ -5,27 +5,62 @@
 	let download = require('download');
 
 	let files = [];
-	let website = 'https://www.math.purdue.edu/~catlin/MA341-spring2018/';
+	let websites = ['https://www.math.purdue.edu/~catlin/MA162/'];
 
-	axios.get(website)
-	    .then((response) => {
-	        if(response.status === 200) {
-	        	const html = response.data;
-	            const $ = cheerio.load(html);
-	            fs.writeFile('index.html', response.data, (err) => console.log('File successfully written!'));
-	            $('a').each(function(i, elem) {
-	            	let link = $(this).attr('href');
-            		files.push(link.substring(2));
-	            });
-	            files.shift();
-	            console.log(files);
-	            files.forEach(function(link) {
-					download(website + link, 'dist').then(() => {
-					    console.log('done!');
+	function scrape(website) {
+		axios.get(website)
+		    .then((response) => {
+		        if(response.status === 200) {
+		        	const html = response.data;
+		            const $ = cheerio.load(html);
+		            fs.writeFile('index.html', response.data, (err) => console.log('File successfully written!'));
+		            $('a').each(function(i, elem) {
+		            	let link = $(this).attr('href');
+		            	if(link != undefined && !link.startsWith("http")) {
+		            		if(link.startsWith("./"))
+		            			files.push(link.substring(2));
+		            		else
+		            			files.push(link);
+		            	}
+		            });
+		            files.forEach(function(link) {
+						download(website + link, 'data').then(() => {
+						    console.log(link + ' : Done');
+						});
 					});
-				});
-				Promise.all([].map(x => download(x, 'dist'))).then(() => {
-				    console.log('files downloaded!');
-				});
-	    	}
-	    }, (error) => console.log(err) );
+					Promise.all([].map(x => download(x, 'data'))).then(() => {
+					    console.log('files downloaded!');
+					});
+		    	}
+		    }, (error) => console.log(err) );
+	}
+
+	function discoverFiles(website) {
+		axios.get(website)
+		    .then((response) => {
+		        if(response.status === 200) {
+		        	const html = response.data;
+		            const $ = cheerio.load(html);
+		            $('a').each(function(i, elem) {
+		            	let link = $(this).attr('href');
+	            		files.push(link);
+		            });
+		            console.log(files);
+		        }
+		    }, (error) => console.log(err) );
+	}
+
+	function save(website) {
+		files.forEach(function(link) {
+			download(website + link, 'data').then(() => {
+			    console.log(link + ' : Done');
+			});
+		});
+		Promise.all([].map(x => download(x, 'data'))).then(() => {
+		    console.log('files downloaded!');
+		});
+	}
+
+	websites.forEach(function(site) {
+		scrape(site);
+	});
